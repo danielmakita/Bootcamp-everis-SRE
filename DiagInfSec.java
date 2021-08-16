75f61cc159e61ba43e7789368fb92d429b6b16fe
Daniel Makita <makita.daniel@gmail.com>
2021-08-16 15:23:10 -0300

    Desafio Aritmetico Java - DiagInfSec

---
+++ DiagInfSec.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DiagInfSec {
+  public static void main(String[] args) throws IOException {
+          Scanner leitor = new Scanner(System.in);
+          double soma = 0;
+          char O = leitor.next().toUpperCase().charAt(0);
+          double[][] M = new double[12][12];
+          int i, j;
+          for (i=0; i<M[0].length; i++) {
+          	for (j=0; j<M[0].length; j++) {
+          		M[i][j] = leitor.nextDouble();
+          	}
+          }
+          
+          for (i=0; i<M[0].length; i++) {
+          	for (j=11; j>=0; j--) {
+          		if (j > M[0].length - i - 1) soma += M[i][j];
+          	}
+          }
+  
+          if (O=='M') soma /= ((M.length * M.length) - 12) / 2; // 66
+      	System.out.println(String.format("%.1f", soma));
+  }
+}
